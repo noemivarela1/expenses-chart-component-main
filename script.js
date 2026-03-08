aad1317f05c63433f5a83d33990b27e5666c69be
Noemi <noemivarela1@gmail.com>
2026-03-08 11:52:29 +0100

    Project developed

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+async function cargarGrafico() {
+    try {
+        // 1. Obtener los datos del archivo local
+        const respuesta = await fetch('data.json');
+        const datos = await respuesta.json();
+
+        const contenedor = document.getElementById('grafico');
+
+        // 2. Calcular el valor máximo para que sea proporcional
+        const maximo = Math.max(...datos.map(d => d.amount));
+
+        // 3. Crear los divs dinámicamente
+        datos.forEach(item => {
+            const barra = document.createElement('div');
+            barra.className = 'barra';
+            if (item.amount === maximo) {
+                barra.classList.add('maxima');
+            }
+            // Altura relativa al valor máximo
+            const altura = (item.amount / maximo) * 100;
+            barra.style.height = `${altura}%`;
+
+            barra.innerHTML = `
+            <span>$${item.amount}</span>
+              <div class="etiqueta">${item.day}</div>
+            `;
+
+            contenedor.appendChild(barra);
+        });
+    } catch (error) {
+        console.error("Error cargando el JSON:", error);
+    }
+}
